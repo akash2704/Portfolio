ea89982fb821173e34074735fbe3d77f86f80afc
Akash <akashkallai27@gmail.com>
2025-01-28 13:02:52 +0530

    modified hambergur sign

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,9 +47,6 @@ function NavBar() {
           <span></span>
           <span></span>
           <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" defaultActiveKey="#home">
